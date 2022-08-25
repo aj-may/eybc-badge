1c66454261c66e9c08ff9c7de4e4d3371c4c2a30
Patrick Macom <patrickmacom@gmail.com>
2022-08-25 14:30:02 -0400

    Added new textures

--- pages/viewer/[tokenId].tsx
+++ pages/viewer/[tokenId].tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from 'next/app'
-import { Canvas } from '@react-three/fiber';
+import { Canvas, extend, useThree } from '@react-three/fiber';
 import CardViewer from 'components/three/cardViewer';
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
+
+extend({ EffectComposer, RenderPass, UnrealBloomPass });
 
 function Viewer({ Component, pageProps }: AppProps) {
+
 
   return (
     <div style={{ background: 'black', height: '100vh', width: '100vw'}}>
@@ -10,10 +16,12 @@ function Viewer({ Component, pageProps }: AppProps) {
           camera={{ position: [1,0,5]}}>
             <ambientLight intensity={.5}/>
             {/* <pointLight position={[10, 10, 10]} /> */}
+          
             <CardViewer
               text="PERSONNAME"
               profileImgSrc='/models/defaultProfile.jpg'
-              />
+            />
+  
         </Canvas>
     </div>
   )
