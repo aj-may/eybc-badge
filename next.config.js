e61aadc904f560660e9cfae90af96b41c36018f9
AJ May <aj7may@gmail.com>
2022-08-27 14:15:41 -0400

    try cors headers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  headers: async () => [
+    {
+      source: '/badges/:path*',
+      headers: [
+        {
+          key: 'Access-Control-Allow-Origin',
+          value: '*'
+        }
+      ]
+    }
+  ]
 };
 
 module.exports = nextConfig;
