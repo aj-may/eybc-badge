0e2f3313ac76a8d43d48a154b9b36a15f516dc64
AJ May <aj7may@gmail.com>
2022-08-28 09:25:43 -0400

    add rewrite for nicer metadata urls

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  rewrites: async () => [
+    {
+      source: '/metadata/:tokenId*',
+      destination: '/api/badges/:tokenId*',
+    },
+  ]
 };
 
 module.exports = nextConfig;
